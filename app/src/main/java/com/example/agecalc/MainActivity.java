bc9101a74a4af04ded921349587fd7a5d953f6d1
Catherine Newman <cathy@newmancj.com>
2017-10-10 13:00:45 +0100

    Fixing no input error

--- app/src/main/java/com/example/agecalc/MainActivity.java
+++ app/src/main/java/com/example/agecalc/MainActivity.java
@@ -21,14 +21,20 @@ public class MainActivity extends Activity implements OnClickListener {
     public void onClick(View view) {
         TextView tv = (TextView) findViewById(R.id.tv1);
         EditText et = (EditText) findViewById(R.id.et1);
-        int yearOfBirth = Integer.parseInt(et.getText().toString());
+        int yearOfBirth = 0;
+        try {
+            yearOfBirth = Integer.parseInt(et.getText().toString());
+        } catch(NumberFormatException ex) {
+            tv.setText("Please enter a year of birth");
+        }
+
+
         int age = 2017 - yearOfBirth;
 
         if (yearOfBirth > 2017) {
             tv.setText("Year of birth cannot be in the future");
-        } else if (yearOfBirth == 0) {
-            tv.setText("Please enter a year of birth");
-        } else {
+
+        } else if (yearOfBirth > 0){
         tv.setText("Your age is: " + String.valueOf(age));}
     }
 }
