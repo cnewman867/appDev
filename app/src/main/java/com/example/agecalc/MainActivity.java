8c7804464a371d970b41099632a9b588b7f9649b
Catherine Newman <cathy@newmancj.com>
2017-10-10 12:44:56 +0100

    homework week 2

--- app/src/main/java/com/example/agecalc/MainActivity.java
+++ app/src/main/java/com/example/agecalc/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.agecalc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnClickListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.btn1);
+        b.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        TextView tv = (TextView) findViewById(R.id.tv1);
+        EditText et = (EditText) findViewById(R.id.et1);
+        int yearOfBirth = Integer.parseInt(et.getText().toString());
+        int age = 2017 - yearOfBirth;
+
+        if (yearOfBirth > 2017) {
+            tv.setText("Year of birth cannot be in the future");
+        } else if (yearOfBirth == 0) {
+            tv.setText("Please enter a year of birth");
+        } else {
+        tv.setText("Your age is: " + String.valueOf(age));}
     }
 }
+
+
